6dc49d70c90ae1e6b52f75d5f15369172d682362
jxu <7989982+jxu@users.noreply.github.com>
2022-12-30 16:50:10 -0500

    Rename some vars

--- timeline.R
+++ timeline.R
@@ -2,22 +2,27 @@ library(tidyverse)
 
 sets_raw <- read_csv("sets.csv")
 themes_raw <- read_csv("themes.csv")
+
 themes <- 
   themes_raw %>% 
   rename(theme_id = id,
          theme_name = name,
-         parent_theme_id = parent_id)
+         parent_theme_id = parent_id) %>% 
+  add_column(ancestor_theme_id = NA)
 
-themes_ancestor <- themes %>% add_column(ancestor_theme_id = NA)
-for (i in 1:nrow(themes_ancestor)) {
-  orig_id <- as.integer(themes[i,"theme_id"])
+for (i in 1:nrow(themes)) {
+  orig_id <- themes[i,]$theme_id
   cur_id <- orig_id
   repeat {
-    par_id <- as.integer(themes[themes$theme_id == cur_id, "parent_theme_id"])
+    par_id <- themes[themes$theme_id == cur_id,]$parent_theme_id
     if (is.na(par_id)) break
     cur_id <- par_id
   }
  
-  themes_ancestor[themes_ancestor$theme_id == orig_id, "ancestor_theme_id"] = cur_id
+  themes[themes$theme_id == orig_id,]$ancestor_theme_id = cur_id
 }
 
+themes <- themes %>%
+  left_join(themes %>% select(theme_id, theme_name),
+            by = c("ancestor_theme_id" = "theme_id")) %>%
+  rename(ancestor_name = theme_name.y)
