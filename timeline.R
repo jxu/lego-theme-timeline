55291f189a6c04a899e1009bb3de6203b9f11a19
jxu <7989982+jxu@users.noreply.github.com>
2022-12-30 18:22:34 -0500

    Clean up recursive parent ID find

--- timeline.R
+++ timeline.R
@@ -3,26 +3,13 @@ library(tidyverse)
 sets_raw <- read_csv("sets.csv")
 themes_raw <- read_csv("themes.csv")
 
-themes <- 
-  themes_raw %>% 
-  rename(theme_id = id,
-         theme_name = name,
-         parent_theme_id = parent_id) %>% 
-  add_column(ancestor_theme_id = NA)
-
-for (i in 1:nrow(themes)) {
-  orig_id <- themes[i,]$theme_id
-  cur_id <- orig_id
-  repeat {
-    par_id <- themes[themes$theme_id == cur_id,]$parent_theme_id
-    if (is.na(par_id)) break
-    cur_id <- par_id
-  }
- 
-  themes[themes$theme_id == orig_id,]$ancestor_theme_id = cur_id
+# recursive parent lookup. credit: zephryl
+find_ancestor <- function(target_id) {
+  dat <- cur_data()
+  parent <- filter(dat, id == target_id)$parent_id
+  if (is.na(parent)) target_id
+  else find_ancestor(parent)
 }
 
-themes <- themes %>%
-  left_join(themes %>% select(theme_id, theme_name),
-            by = c("ancestor_theme_id" = "theme_id")) %>%
-  rename(ancestor_name = theme_name.y)
+themes <- themes_raw %>% 
+  mutate(ancestor_theme_id = map_dbl(id, find_ancestor)) 
