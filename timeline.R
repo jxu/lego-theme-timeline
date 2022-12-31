355f1465de72de8f6fa05645fd5d56da8551c16d
jxu <7989982+jxu@users.noreply.github.com>
2022-12-31 13:24:32 -0500

    Clean up and save plot

--- timeline.R
+++ timeline.R
@@ -36,11 +36,31 @@ theme_years <- sets %>%
   summarize(rr = run_range_cols()) %>%
   unpack(cols = rr) 
 
+# sort theme by introduction year
+theme_intro <- theme_years %>%
+  group_by(theme) %>%
+  summarize(intro = min(start)) %>%
+  arrange(intro)
+
+selected_theme_order <- theme_intro %>%
+  filter(theme %in% selected_themes$theme) %>%
+  pull(theme)
+
 # plot selected themes
+color_map <- selected_themes$color
+names(color_map) <- selected_themes$theme
+
 theme_years %>%
   filter(theme %in% selected_themes$theme) %>%
   # end+1 to show bars until end of year
   ggplot(aes(x=start, xend=end+1, y=theme, yend=theme, color=theme)) +
-  geom_segment(size=2, show.legend = FALSE) + 
-  scale_y_discrete(limits=rev) + 
-  theme_bw()
+  geom_segment(linewidth=3, show.legend = FALSE) + 
+  scale_y_discrete(limits=rev(selected_theme_order), position="right") + 
+  scale_x_continuous(expand = c(0,0), breaks=seq(1950, 2030, 10)) +
+  coord_cartesian(xlim=c(1945,2023)) + 
+  scale_color_manual(values=color_map) +
+  labs(title="Selected LEGO Themes Timeline", x="Year", y="Theme") +
+  theme_bw() + 
+  theme(plot.title = element_text(hjust = 0.5))
+
+ggsave("selected_timeline.png", width=2000, height=1500, units="px")
