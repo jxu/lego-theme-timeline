be648a7c91edfa78cd36d705c4286495cbc11498
jxu <7989982+jxu@users.noreply.github.com>
2022-12-31 12:13:53 -0500

    Add initial testing plot

--- timeline.R
+++ timeline.R
@@ -1,8 +1,10 @@
 library(IRanges)
 library(tidyverse)
 
-sets_raw <- read_csv("sets.csv")
-themes_raw <- read_csv("themes.csv")
+# data import
+sets_raw        <- read_csv("sets.csv")
+themes_raw      <- read_csv("themes.csv")
+selected_themes <- read_csv("selected_themes.csv")
 
 # recursive parent lookup. credit: zephryl
 find_ancestor <- function(target_id) {
@@ -12,6 +14,7 @@ find_ancestor <- function(target_id) {
   else find_ancestor(parent)
 }
 
+# add (ancestor) theme name to sets table
 themes <- themes_raw %>% 
   mutate(ancestor_id = map_dbl(id, find_ancestor))
 
@@ -21,15 +24,23 @@ themes <- themes %>%
 sets <- sets_raw %>%
   left_join(themes %>% select(theme_id = id, ancestor_name))
 
-
+# compute year run ranges
 run_range_cols <- function() {
   y <- cur_data()$year
   y <- y %>% unique %>% sort
   IRanges::reduce(IRanges(y)) %>% data.frame %>% select(start, end)
 }
 
-
-sets %>% 
+theme_years <- sets %>% 
   group_by(theme = ancestor_name) %>%
   summarize(rr = run_range_cols()) %>%
   unpack(cols = rr) 
+
+# plot selected themes
+theme_years %>%
+  filter(theme %in% selected_themes$theme) %>%
+  # end+1 to show bars until end of year
+  ggplot(aes(x=start, xend=end+1, y=theme, yend=theme, color=theme)) +
+  geom_segment(size=2, show.legend = FALSE) + 
+  scale_y_discrete(limits=rev) + 
+  theme_bw()
