27ae29d7f280cf8e2f915ae1d613074d82c90f20
jxu <7989982+jxu@users.noreply.github.com>
2022-12-31 16:39:54 -0500

    Add all themes timeline

--- timeline.R
+++ timeline.R
@@ -19,10 +19,10 @@ themes <- themes_raw %>%
   mutate(ancestor_id = map_dbl(id, find_ancestor))
 
 themes <- themes %>%
-  left_join(themes %>% select(ancestor_id = id, ancestor_name = name))
+  left_join(select(themes, ancestor_id = id, ancestor_name = name))
 
 sets <- sets_raw %>%
-  left_join(themes %>% select(theme_id = id, ancestor_name))
+  left_join(select(themes, theme_id = id, ancestor_name))
 
 # compute year run ranges
 run_range_cols <- function() {
@@ -42,6 +42,7 @@ theme_intro <- theme_years %>%
   summarize(intro = min(start)) %>%
   arrange(intro)
 
+theme_order <- theme_intro$theme
 selected_theme_order <- theme_intro %>%
   filter(theme %in% selected_themes$theme) %>%
   pull(theme)
@@ -50,6 +51,9 @@ selected_theme_order <- theme_intro %>%
 color_map <- selected_themes$color
 names(color_map) <- selected_themes$theme
 
+# simple ggplot theming with centered title
+theme_set(theme_bw() + theme(plot.title = element_text(hjust = 0.5)))
+
 theme_years %>%
   filter(theme %in% selected_themes$theme) %>%
   # end+1 to show bars until end of year
@@ -57,10 +61,19 @@ theme_years %>%
   geom_segment(linewidth=3, show.legend = FALSE) + 
   scale_y_discrete(limits=rev(selected_theme_order), position="right") + 
   scale_x_continuous(expand = c(0,0), breaks=seq(1950, 2030, 10)) +
-  coord_cartesian(xlim=c(1945,2023)) + 
+  coord_cartesian(xlim=c(1945,2024)) + 
   scale_color_manual(values=color_map) +
-  labs(title="Selected LEGO Themes Timeline", x="Year", y="Theme") +
-  theme_bw() + 
-  theme(plot.title = element_text(hjust = 0.5))
+  labs(title="Selected LEGO Themes Timeline", x="Year", y="Theme")
 
-ggsave("selected_timeline.png", width=2000, height=1500, units="px")
+ggsave("selected_timeline.png", width=3000, height=1500, units="px")
+
+# plot all themes in giant timeline
+theme_years %>%
+  ggplot(aes(x=start, xend=end+1, y=theme, yend=theme, color=theme)) + 
+  geom_segment(linewidth=3, show.legend=F) +
+  scale_y_discrete(limits=rev(theme_order), position="right") + 
+  scale_x_continuous(expand = c(0,0), breaks=seq(1950, 2030, 10)) +
+  coord_cartesian(xlim=c(1945,2024)) +
+  labs(title="All LEGO Themes Timeline", x="Year", y="Theme")
+
+ggsave("all_timeline.png", width=3000, height=5000, units="px")
